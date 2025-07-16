c99bdf7ca3d32c036d670e223fcfb27eb15c7d4a
sathesh-90 <r210747@rguktrkv.ac.in>
2025-07-16 09:09:10 +0530

    day-8

--- ThreeSum.java
+++ ThreeSum.java
@@ -8,7 +8,8 @@ public class ThreeSum {
         int n = nums.length;
 
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
 
             int target = -nums[i];
             int left = i + 1;
@@ -20,8 +21,10 @@ public class ThreeSum {
                 if (sum == target) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1]) 
+                        right--;
 
                     left++;
                     right--;
